78a4297da21603f655b95c6d84a17f5090cfd0fb
Sandeep Mistry <sandeep.mistry@arm.com>
2023-04-24 10:34:11 -0400

    Add support for optionally setting the AVH API endpoint via AVH_API_ENDPOINT env var

--- scripts/tests/avh/test_lighting_app.py
+++ scripts/tests/avh/test_lighting_app.py
@@ -46,7 +46,12 @@ class TestLightingApp(unittest.TestCase):
         )
         self.logger.addHandler(stdout_logger_handler)
 
-        self.avh_client = AvhClient(os.environ["AVH_API_TOKEN"])
+        self.avh_client = AvhClient(
+            api_token=os.environ["AVH_API_TOKEN"],
+            api_endpoint=os.environ["AVH_API_ENDPOINT"]
+            if "AVH_API_ENDPOINT" in os.environ
+            else None,
+        )
 
         self.chip_tool_instance = AvhChiptoolInstance(
             self.avh_client,
